e7339f6c9dc474164397551a403858361594d977
ilia_uvarov_1 <streamrobdiez@mail.ru>
2025-02-16 18:57:32 +0300

    add annotated for allowed hosts field

--- mysite/config.py
+++ mysite/config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from typing import Annotated
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 env_path = Path(__file__).resolve().parent.parent.joinpath(".env")
@@ -9,7 +11,7 @@ class Settings(BaseSettings):
     secret_key_django: str | None = None
     loglevel: str | None = None
     django_debug: str | None = None
-    allowed_hosts: str | None = None
+    allowed_hosts: Annotated[str, Field(default="localhost")]
     model_config = SettingsConfigDict(env_file=env_path, env_file_encoding='utf-8')
 
 
