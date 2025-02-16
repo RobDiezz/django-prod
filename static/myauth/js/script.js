778777698f72fc8e573e320c79dcc24733ad6813
ilia_uvarov_1 <streamrobdiez@mail.ru>
2025-02-16 19:42:27 +0300

    delete static

--- static/myauth/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('backToProfileBtn').addEventListener('click', function () {
-        const pk = this.getAttribute("data-pk");
-        const url = `/accounts/about-me/${pk}/`;
-        window.location.href = url;  // Перенаправляем на сформированный URL
-    });
-});
